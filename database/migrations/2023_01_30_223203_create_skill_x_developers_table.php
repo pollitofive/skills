6cea6011d664cff203c3a972e7f9061b34a03557
Damian <damianladiani@gmail.com>
2023-01-30 19:49:52 -0300

    First commit
    Migrations created

---
+++ database/migrations/2023_01_30_223203_create_skill_x_developers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skill_x_developers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('skill_id')->constrained('skills');
+            $table->foreignId('developer_id')->constrained('developers');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skill_x_developers');
+    }
+};
