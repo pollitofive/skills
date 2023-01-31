29b755e66dee1031a528ba8f165e2b03d1ce3d62
Damian <damianladiani@gmail.com>
2023-01-31 18:44:26 -0300

    Adding the top bar

---
+++ resources/views/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="horizontal" data-topbar="dark" data-sidebar-size="lg" data-sidebar="light" data-sidebar-image="none" data-preloader="disable">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/icons.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/custom.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/swiper-bundle.min.css') }}">
+        @livewireStyles
+
+        <style>
+            .logo {
+                color: white;
+                font-size: 24px;
+                font-weight: bold;
+            }
+
+        </style>
+    </head>
+    <body class="antialiased">
+        <div id="layout-wrapper">
+            <header id="page-topbar" class="">
+                <div class="layout-width">
+                    <div class="navbar-header">
+                        <div class="d-flex">
+                            <div class="navbar-brand-box horizontal-logo">
+                                <span class="logo">
+                                    Skills by developers
+                                </span>
+                            </div>
+                        </div>
+{{--                        @include('user')--}}
+                    </div>
+                </div>
+            </header>
+        </div>
+        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+        @livewireScripts
+    </body>
+</html>
