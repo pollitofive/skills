358cf0cf105356996da44e60036013c8404f6344
Damian <damianladiani@gmail.com>
2023-02-15 20:08:16 -0300

    Implemented the function of logout

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,32 +4,51 @@
             <div class="d-flex">
                 <!-- LOGO -->
                 <div class="navbar-brand-box horizontal-logo">
-                    <a href="index.html" class="logo logo-dark">
-                                    <span class="logo-sm">
-                                        Skills by developers
-                                    </span>
+                    <a href="{{ route('developers') }}" class="logo logo-light">
+                        <span class="logo-sm">
+                            Skills by developers
+                        </span>
                         <span class="logo-lg">
-                                        Skills by developers
-                                    </span>
-                    </a>
-
-                    <a href="index.html" class="logo logo-light">
-                                <span class="logo-sm">
-                                    Skills by developers
-                                </span>
-                        <span class="logo-lg">
-                                    Skills by developers
-                                </span>
+                            Skills by developers
+                        </span>
                     </a>
                 </div>
 
                 <button type="button" class="btn btn-sm px-3 fs-16 header-item vertical-menu-btn topnav-hamburger" id="topnav-hamburger-icon">
-                            <span class="hamburger-icon">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </span>
+                    <span class="hamburger-icon">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
                 </button>
+
+            </div>
+            <div class="d-flex align-items-center">
+                <div class="dropdown ms-sm-3 header-item topbar-user">
+                    <button type="button" class="btn" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="d-flex align-items-center">
+                                <span class="text-start ms-xl-2">
+                                    <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{ auth()->user()->name }}</span>
+                                </span>
+                            </span>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <!-- item-->
+                        <h6 class="dropdown-header">Welcome {{ auth()->user()->name }}!</h6>
+                        <a class="dropdown-item" href="pages-profile.html">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-rotate-cw"><polyline points="23 4 23 10 17 10"></polyline><path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10"></path></svg>
+                            <span class="align-middle">Change password</span>
+                        </a>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button class="dropdown-item"onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-power"><path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path><line x1="12" y1="2" x2="12" y2="12"></line></svg>
+                                <span class="align-middle">  Logout</span>
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
