4c63af452a735736dc0bf05ca631eeb2b6a2f60c
Damian <damianladiani@gmail.com>
2023-02-15 19:46:20 -0300

    Adding login functions

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('auth.login');
+})->middleware('auth.isLoggedIn');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/developers', function () {
+        return view('developers');
+    })->name('developers');
+
+    Route::get('/skills', function () {
+        return view('skills');
+    })->name('skills');
+
 });
 
-require __DIR__.'/auth.php';
-
-Route::get('/developers', function () {
-    return view('developers');
-})->name('developers');
-
-Route::get('/skills', function () {
-    return view('skills');
-})->name('skills');
